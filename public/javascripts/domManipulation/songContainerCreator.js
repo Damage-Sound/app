b601fd26518cf6f2fe9dadfafbf993cc565f577f
togeri <gerartoledo@gmail.com>
2020-04-29 15:12:08 +0200

    more logging

--- public/javascripts/domManipulation/songContainerCreator.js
+++ public/javascripts/domManipulation/songContainerCreator.js
@@ -3,24 +3,34 @@ const songContainerCreator = (song) => {
 
     const songContainer = document.createElement('div')
     songContainer.classList += 'song-container'
+    console.log('CONTAINER CREATED')
+    console.log(songContainer)
 
     const songTitle = document.createElement('h3')
     songTitle.classList += 'song-title'
     songTitle.innerHTML = song.name
+    console.log('TITLE CREATED')
+    console.log(songTitle)
 
     const songAuthor = document.createElement('h4')
     songAuthor.classList += 'song-author'
     songAuthor.innerHTML = song.author
+    console.log('Author CREATED')
+    console.log(songAuthor)
 
     const songAlbum = document.createElement('h4')
     songAlbum.classList += 'song-album'
     songAlbum.innerHTML = song.album
+    console.log('Album CREATED')
+    console.log(songAlbum)
 
     const playButton = document.createElement('button')
     playButton.setAttribute('type', 'submit')
     playButton.classList.add('btn btn-dark btn-block btn-lg playButton')
     playButton.setAttribute('media', song.url)
     playButton.innerHTML = 'Play'
+    console.log('Button CREATED')
+    console.log(songButton)
 
     songContainer.appendChild(playButton)
     songContainer.appendChild(songTitle)
