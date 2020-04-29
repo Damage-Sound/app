b063167671f42122ea23cd895928a565c94773be
togeri <gerartoledo@gmail.com>
2020-04-29 16:56:15 +0200

    Add Playlist Axios

--- public/javascripts/profilePlaylists.js
+++ public/javascripts/profilePlaylists.js
@@ -1,28 +1,67 @@
 const profilePlaylists = (data) => {
 
     const container = document.querySelector('.profile-actions')
+    console.log('entra en profilePlatlists function con esta data:')
+    console.log(data);
 
     data.forEach(playlist => {
-        playlist.songs.forEach(song => {
+        console.log('para esta playlist:')
+        console.log(playlist);
 
-            const playlistContainer = document.createElement('div')
-            playlistContainer.classList += 'playlist-container'
-            
-            const playlistTitle = document.createElement('h3')
-            playlistTitle.classList += 'playlist-title'
-            playlistTitle.innerHTML = playlist.name
+        const playlistContainer = document.createElement('div')
+        playlistContainer.classList += 'playlist-container'
 
-            const playlistLikes = document.createElement('p')
-            playlistLikes.classList += 'playlist-likes'
-            playlistLikes.innerHTML = playlist.likes
+        const playlistTitle = document.createElement('h2')
+        playlistTitle.classList += 'playlist-title'
+        playlistTitle.innerHTML = playlist.name
 
-            const playlistPlays = document.createElement('p')
-            playlistPlays.classList += 'playlist-plays'
-            playlistPlays.innerHTML = playlist.plays.total
+        const playlistLikes = document.createElement('p')
+        playlistLikes.classList += 'playlist-likes'
+        playlistLikes.innerHTML = playlist.likes
 
-            playlistContainer.appendChild(songContainerCreator(song))
-            container.appendChild(playListContainer)
-        })
+        const playlistPlays = document.createElement('p')
+        playlistPlays.classList += 'playlist-plays'
+        playlistPlays.innerHTML = playlist.plays.total
+
+        playlistContainer.appendChild(playlistTitle)
+        playlistContainer.appendChild(playlistLikes)
+        playlistContainer.appendChild(playlistPlays)
+
+        container.appendChild(playListContainer)
+
+
+        // const divPlaylistInfo = document.createElement('div')
+        // divPlaylistInfo.classList += 'playlist-info'
+
+        // const divPlaylistSongs = document.createElement('div')
+        // divPlaylistSongs.classList += 'playlist-songs'
+
+        // const playlistTitle = document.createElement('h2')
+        // playlistTitle.classList += 'playlist-title'
+        // playlistTitle.innerHTML = playlist.name
+
+        // const playlistLikes = document.createElement('p')
+        // playlistLikes.classList += 'playlist-likes'
+        // playlistLikes.innerHTML = playlist.likes
+
+        // const playlistPlays = document.createElement('p')
+        // playlistPlays.classList += 'playlist-plays'
+        // playlistPlays.innerHTML = playlist.plays.total
+
+        // divPlaylistInfo.appendChild(playlistTitle)
+        // divPlaylistInfo.appendChild(playlistLikes)
+        // divPlaylistInfo.appendChild(playlistPlays)
+
+        // playlistContainer.appendChild(divPlaylistInfo)
+
+        // playlist.songs.forEach(song => {
+
+        //     console.log('para esta cancion');
+        //     console.log(song);
+
+        //     playlistContainer.appendChild(songContainerCreator(song))
+        //     container.appendChild(playListContainer)
+        // })
     });
 }
 
