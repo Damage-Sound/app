01126bf95a3b83db477d83ac8a84e95fd30da4fc
Madalin George <madalin.lingurar@gmail.com>
2020-04-29 11:03:37 +0200

    push & pull 29 abril

--- public/javascripts/webplayer.js
+++ public/javascripts/webplayer.js
@@ -13,5 +13,6 @@ window.onload = () => {
 const play = (event) => {
     const player = document.querySelector('audio')
     player.src = event.target.getAttribute('media')
+    console.log(player)
     player.play()
 }
