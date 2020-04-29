9670a821015e841c9cce5bd81a34fe723580604c
togeri <gerartoledo@gmail.com>
2020-04-29 19:59:58 +0200

    Album AXIOS

--- public/javascripts/domManipulation/profileAlbums.js
+++ public/javascripts/domManipulation/profileAlbums.js
@@ -7,16 +7,40 @@ const profileAlbums = (data) => {
         const albumContainer = document.createElement('div')
         albumContainer.classList += 'album-container'
 
+        const albumCoverContainer = document.createElement('div')
+        albumCoverContainer.classList += 'album-cover-container'
+
+        const albumInfoContainer = document.createElement('div')
+        albumInfoContainer.classList += 'album-info-container'
+
+        const albumCover = document.createElement('img')
+        albumCover.classList += 'album-cover'
+        albumCover.setAttribute('src', album.cover)
+
         const albumTitle = document.createElement('h2')
         albumTitle.classList += 'album-title'
         albumTitle.innerHTML = album.name
+
+        const albumGenreContainer = document.createElement('div')
+        albumGenreContainer.classList += 'album-genre-container'
+        album.genre.forEach(genre => {
+            const genre = document.createElement('p')
+            genre.classList += 'album-genre'
+            albumGenreContainer.appendChild(genre)
+        })
 
         const albumLikes = document.createElement('p')
         albumLikes.classList += 'album-likes'
         albumLikes.innerHTML = album.likes
 
-        albumContainer.appendChild(albumTitle)
-        albumContainer.appendChild(albumLikes)
+        albumCoverContainer.appendChild(albumCover)
+
+        albumInfoContainer.appendChild(albumTitle)
+        albumInfoContainer.appendChild(albumGenreContainer)
+        albumInfoContainer.appendChild(albumLikes)
+
+        albumContainer.appendChild(albumCoverContainer)
+        albumContainer.appendChild(albumInfoContainer)
 
         container.appendChild(albumContainer)
     })
