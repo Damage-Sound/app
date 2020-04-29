cb73b91cf15ad01564d09c0c0ab918f394283cab
togeri <gerartoledo@gmail.com>
2020-04-29 14:25:26 +0200

    Fix missplaced backtick

--- public/javascripts/profilePlaylists.js
+++ public/javascripts/profilePlaylists.js
@@ -1,7 +1,7 @@
 const profilePlaylists = (data) => {
     console.log('de Estamos en a profilePlaylists')
     console.log('data: ', data)
-    const container = document.querySelector('.profile-actions"')
+    const container = document.querySelector('.profile-actions')
     console.log(container)
     console.log(data)
 }
