3daf03fcc007212a1284ade4e8079629ce93c2d8
togeri <gerartoledo@gmail.com>
2020-05-01 00:33:16 +0200

    Fixed url

--- public/javascripts/likeMiddleware.js
+++ public/javascripts/likeMiddleware.js
@@ -5,7 +5,7 @@ const likeMiddleware = (event) => {
     const apiMethods = new APIHandler(`http://damage-sound.herokuapp.com/api/like/${song.getAttribute('songID')}`)
 
     apiMethods.getInfo()
-        .then(response => Array.from(song.parentNode.childNodes).filter(node => node.className === 'song-likes')[0].innerHTML = response)
+        .then(response => Array.from(song.parentNode.childNodes).filter(node => node.className === 'song-likes')[0].innerHTML = `${response} <img class="flaticon-img" src="/images/heart2.svg" alt="like">`)
         .catch(error => console.log(error))
 }
 
