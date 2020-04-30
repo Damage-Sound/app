5641d18e4f446c12133cf081b78bebda8f66bc61
togeri <gerartoledo@gmail.com>
2020-05-01 00:37:41 +0200

    Fixed emojis

--- public/javascripts/playMiddleware.js
+++ public/javascripts/playMiddleware.js
@@ -4,6 +4,6 @@ const playMiddleware = (song) => {
 
 
     apiMethods.getInfo()
-        .then(response => Array.from(song.parentNode.childNodes).filter(node => node.className === 'song-plays')[0].innerHTML = response)
+        .then(response => Array.from(song.parentNode.childNodes).filter(node => node.className === 'song-plays')[0].innerHTML = `${response} <img class="flaticon-img" src="/images/heart2.svg" alt="like">`)
         .catch(error => console.log(error))
 }
