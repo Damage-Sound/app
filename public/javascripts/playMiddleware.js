b4e44d62758d604ac73e0ba753133bdab1073802
togeri <gerartoledo@gmail.com>
2020-04-30 19:56:51 +0200

    Add middleware to discover

--- public/javascripts/playMiddleware.js
+++ public/javascripts/playMiddleware.js
@@ -4,10 +4,6 @@ const playMiddleware = (song) => {
 
 
     apiMethods.getInfo()
-        .then(response => {
-            // let plays = song.parentNode.childNodes[2]
-            // plays.innerHTML = response
-            Array.from(song.parentNode.childNodes).filter(node => node.nodeType !== Node.TEXT_NODE)[2].innerHTML = response
-        })
+        .then(response => Array.from(song.parentNode.childNodes).filter(node => node.nodeType !== Node.TEXT_NODE)[2].innerHTML = response)
         .catch(error => console.log(error))
 }
