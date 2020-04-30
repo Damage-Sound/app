a92ab0c62ba26c180b2db637d5c4f25896e286f9
togeri <gerartoledo@gmail.com>
2020-04-30 12:03:59 +0200

    Play Like Middleware deploy to Heroku

--- routes/API-requests/data-handling.routes.js
+++ routes/API-requests/data-handling.routes.js
@@ -48,18 +48,16 @@ router.get('/', (req, res, next) => {
 // Likes counter updater
 router.get('/like/:id', (req, res, next) => {
 
-    console.log('HOLA')
-
     const songID = req.params.id
 
     Song.findById(songID)
         .then(foundSong => {
             const likes = foundSong.likes + 1
             return Song.findByIdAndUpdate(foundSong.id, { likes }, { new: true })
-                .then(response => console.log(response))
+                .then(response => res.json(response))
                 .catch(error => console.log('error: ', error))
         })
-        .then(response => { console.log(response) })
+        .then(response => res.json(response))
         .catch(error => console.log(error))
 })
 
