9dcc7443516fdd44c08b7ae7a69c505920add013
togeri <gerartoledo@gmail.com>
2020-04-30 13:49:41 +0200

    Play Like Middleware deploy to Heroku

--- public/javascripts/likeMiddleware.js
+++ public/javascripts/likeMiddleware.js
@@ -1,16 +1,16 @@
-// class APIHandler {
-//     constructor(baseUrl) {
-//         this.BASE_URL = baseUrl;
-//         this.axiosAPI = axios.create({ baseUrl: baseUrl })
-//     }
+class APIHandler {
+    constructor(baseUrl) {
+        this.BASE_URL = baseUrl;
+        this.axiosAPI = axios.create({ baseUrl: baseUrl })
+    }
 
-//     getLikes() {
-//         return this.axiosAPI.get(this.BASE_URL)
-//             .then(result => result.data)
-//             .catch(error => console.log(error))
-//     }
+    getLikes() {
+        return this.axiosAPI.get(this.BASE_URL)
+            .then(result => result.data)
+            .catch(error => console.log(error))
+    }
 
-// }
+}
 
 
 // apiMethods.getLikes()
@@ -19,34 +19,34 @@
 
 
 
-const likeMiddleware = (event) => {
+// const likeMiddleware = (event) => {
     
+//     const song = event.target
+
+//     // event.preventDefault()
+
+//     axios({
+//         method: 'get',
+//         url: `http://www.damage-sound.herokuapp.com/api/like/5ea98f3ef9b7c12555c4e793`
+//     })
+//         .then(response => console.log(response))
+//         .catch(err => console.log(err))
+// }
+
+const likeMiddleware = (event) => {
+
+    console.log(event)
+
     const song = event.target
 
     // event.preventDefault()
 
-    axios({
-        method: 'get',
-        url: `http://www.damage-sound.herokuapp.com/api/like/5ea98f3ef9b7c12555c4e793`
-    })
-        .then(response => console.log(response))
-        .catch(err => console.log(err))
-}
-
-// const likeMiddleware = (event) => {
-
-//     console.log(event)
-
-//     const song = event.target
-
-//     event.preventDefault()
-
-//     const apiMethods = new APIHandler(`http://www.damage-sound.herokuapp.com/api/like/${song.getAttribute('songID')}`)
+    const apiMethods = new APIHandler(`http://www.damage-sound.herokuapp.com/api/like/${song.getAttribute('songID')}`)
     
-//     apiMethods.getLikes()
-//         .then(response => console.log(response.data))
-//         .catch(error => console.log(error))
-// }
+    apiMethods.getLikes()
+        .then(response => console.log(response.data))
+        .catch(error => console.log(error))
+}
 
 
 
