514680b85764ed1f19c309ee9ecac3fd9450bb37
togeri <gerartoledo@gmail.com>
2020-04-30 11:46:53 +0200

    Play Like Middleware deploy to Heroku

--- public/javascripts/webplayerSPA.js
+++ public/javascripts/webplayerSPA.js
@@ -1,7 +1,7 @@
-// const play = (event) => { 
-//     const player = document.querySelector('audio')
-//     player.src = event.target.getAttribute('media')
-//     player.play()
-// }
+const play = (event) => { 
+    const player = document.querySelector('audio')
+    player.src = event.target.getAttribute('media')
+    player.play()
+}
 
-// const like = (event) => likeMiddleware(event.target)
+const like = (event) => likeMiddleware(event.target)
