1f0b61e937daf709ff52f1e7fa31678c86d0f693
togeri <gerartoledo@gmail.com>
2020-04-30 11:11:49 +0200

    Switch query to params in axios middleware

--- public/javascripts/likeMiddleware.js
+++ public/javascripts/likeMiddleware.js
@@ -4,7 +4,7 @@ const like = (event) => likeMiddleware(event.target)
 const likeMiddleware = (song) => {
     axios({
         method: 'get',
-        url: `http://www.damage-sound.herokuapp.com/api/like/?song=${song.getAttribute('songID')}`
+        url: `http://www.damage-sound.herokuapp.com/api/like/${song.getAttribute('songID')}`
     })
         .then(response => console.log(response))
         .catch(err => console.log(err))
--- public/javascripts/playMiddleware.js
+++ public/javascripts/playMiddleware.js
@@ -3,7 +3,7 @@
 const playMiddleware = (song) => {
     axios({
         method: 'get',
-        url: `http://www.damage-sound.herokuapp.com/api/play/?song=${song.getAttribute('songID')}`
+        url: `http://www.damage-sound.herokuapp.com/api/play/${song.getAttribute('songID')}`
     })
         .then(response => console.log(response))
         .catch(err => console.log(err))
