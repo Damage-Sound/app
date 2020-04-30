2a94b05070c9f5095f93981d89862736ef8e09d2
Madalin George <madalin.lingurar@gmail.com>
2020-04-30 18:33:43 +0200

    push

--- app.js
+++ app.js
@@ -28,5 +28,6 @@ app.use('/api', require('./routes/API-requests/data-handling.routes'))
 app.use('/album', require('./routes/album-detail'))
 app.use('/playlist', require('./routes/playlist-detail'))
 app.use('/discover', require('./routes/discover.routes'))
+app.use('/artist', require('./routes/artist.routes'))
 app.use('/colab', require('./routes/colab.routes'))
 module.exports = app
