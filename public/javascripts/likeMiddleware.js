84b6a705aa1d46505af8148a51dc34b5e12e6e00
togeri <gerartoledo@gmail.com>
2020-04-30 12:38:33 +0200

    Play Like Middleware deploy to Heroku

--- public/javascripts/likeMiddleware.js
+++ public/javascripts/likeMiddleware.js
@@ -1,16 +1,16 @@
-class APIHandler {
-    constructor(baseUrl) {
-        this.BASE_URL = baseUrl;
-        this.axiosAPI = axios.create({ baseUrl: baseUrl })
-    }
+// class APIHandler {
+//     constructor(baseUrl) {
+//         this.BASE_URL = baseUrl;
+//         this.axiosAPI = axios.create({ baseUrl: baseUrl })
+//     }
 
-    getLikes() {
-        return this.axiosAPI.get(this.BASE_URL)
-            .then(result => result.data)
-            .catch(error => console.log(error))
-    }
+//     getLikes() {
+//         return this.axiosAPI.get(this.BASE_URL)
+//             .then(result => result.data)
+//             .catch(error => console.log(error))
+//     }
 
-}
+// }
 
 
 // apiMethods.getLikes()
@@ -19,29 +19,34 @@ class APIHandler {
 
 
 
-// const likeMiddleware = (song) => {
-//     axios({
-//         method: 'get',
-//         url: `http://www.damage-sound.herokuapp.com/api/like/${song.getAttribute('songID')}`
-//     })
-//         .then(response => console.log(response))
-//         .catch(err => console.log(err))
-// }
-
 const likeMiddleware = (event) => {
-
-    console.log(event)
-
+    
     const song = event.target
 
     event.preventDefault()
 
-    const apiMethods = new APIHandler(`http://www.damage-sound.herokuapp.com/api/like/${song.getAttribute('songID')}`)
-    
-    apiMethods.getLikes()
-        .then(response => console.log(response.data))
-        .catch(error => console.log(error))
+    axios({
+        method: 'get',
+        url: `http://www.damage-sound.herokuapp.com/api/like/${song.getAttribute('songID')}`
+    })
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
 }
+
+// const likeMiddleware = (event) => {
+
+//     console.log(event)
+
+//     const song = event.target
+
+//     event.preventDefault()
+
+//     const apiMethods = new APIHandler(`http://www.damage-sound.herokuapp.com/api/like/${song.getAttribute('songID')}`)
+    
+//     apiMethods.getLikes()
+//         .then(response => console.log(response.data))
+//         .catch(error => console.log(error))
+// }
 
 
 
