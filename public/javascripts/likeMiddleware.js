add5e969a42f6904764d34470fceb69b662af4cd
togeri <gerartoledo@gmail.com>
2020-04-30 12:20:24 +0200

    Play Like Middleware deploy to Heroku

--- public/javascripts/likeMiddleware.js
+++ public/javascripts/likeMiddleware.js
@@ -1,11 +1,41 @@
-const likeMiddleware = (song) => {
-    axios({
-        method: 'get',
-        url: `http://www.damage-sound.herokuapp.com/api/like/${song.getAttribute('songID')}`
-    })
-    .then(response => console.log(response))
-    .catch(err => console.log(err))
+class APIHandler {
+    constructor(baseUrl) {
+        this.BASE_URL = baseUrl;
+        this.axiosAPI = axios.create({ baseUrl: baseUrl })
+    }
+
+    getLikes() {
+        return this.axiosAPI.get(this.BASE_URL)
+            .then(result => result.data)
+            .catch(error => console.log(error))
+    }
+
 }
+
+const apiMethods = new APIHandler(`http://www.damage-sound.herokuapp.com/api/like/${song.getAttribute('songID')}`)
+
+apiMethods.getLikes()
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error))
+
+
+
+// const likeMiddleware = (song) => {
+//     axios({
+//         method: 'get',
+//         url: `http://www.damage-sound.herokuapp.com/api/like/${song.getAttribute('songID')}`
+//     })
+//         .then(response => console.log(response))
+//         .catch(err => console.log(err))
+// }
+
+const likeMiddleware = (song) => {
+    apiMethods.getLikes()
+        .then(response => console.log(response.data))
+        .catch(error => console.log(error))
+}
+
+
 
 
 const like = (event) => likeMiddleware(event.target)
