62e2dfd5c77df98ac7603eb21b5b48ec9fed6ab2
togeri <gerartoledo@gmail.com>
2020-05-01 10:24:33 +0200

    profile playlists styles

--- public/javascripts/domManipulation/profilePlaylists.js
+++ public/javascripts/domManipulation/profilePlaylists.js
@@ -1,11 +1,13 @@
 const profilePlaylists = (data) => {
 
     const container = document.querySelector('.profile-actions')
+    const playlistRow = document.createElement('div')
+    playlistRow.classList += 'row albums-row'
 
     data.forEach(playlist => {
 
         const playlistContainer = document.createElement('div')
-        playlistContainer.classList += 'playlist-container'
+        playlistContainer.classList += 'playlist-container col-md-3'
 
         const playlistTitle = document.createElement('h2')
         playlistTitle.classList += 'playlist-title'
@@ -18,14 +20,14 @@ const profilePlaylists = (data) => {
         playlistContainer.appendChild(playlistTitle)
         playlistContainer.appendChild(playlistLikes)
 
-        
         const linkToPlaylist = document.createElement('a')
         linkToPlaylist.classList += 'playlist-link'
         linkToPlaylist.setAttribute('href', `http://damage-sound.herokuapp.com/playlist/${playlist._id}`)
-        
-        linkToPlaylist.appendChild(playlistContainer)
-        container.appendChild(linkToPlaylist)
 
+        linkToPlaylist.appendChild(playlistContainer)
+        playlistRow.appendChild(linkToPlaylist)
     })
+
+    container.appendChild(playlistRow)
 }
 
